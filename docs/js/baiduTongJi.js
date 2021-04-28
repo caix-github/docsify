6a793985431e0075f0169355cd9b5f957b843d3b
蔡鑫 <1058360098@qq.com>
2021-04-28 13:50:52 +0800

    追加百度统计

---
+++ docs/js/baiduTongJi.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a3418df139a98b78e1dc45697a8b7f96";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
